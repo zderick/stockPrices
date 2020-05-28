d251e16109167d9e1fb102aa2b85f7586d377ad3
Derick <centuryib100@gmail.com>
2020-05-28 19:21:06 +0000

    Add DOW index

--- stock.py
+++ stock.py
@@ -4,12 +4,33 @@ from email.header import Header
 
 from email.MIMEText import MIMEText
 from email.utils import formataddr
- 
+import json
+
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+
+def get_jsonparsed_data(url):
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+url = ("https://financialmodelingprep.com/api/v3/quote/%5EDJI?apikey=a9a0e2e937837eb7414a9dee88b2f09a")
+
+name = get_jsonparsed_data(url)[0]['name']
+price = get_jsonparsed_data(url)[0]['price']
+changesPercentage = get_jsonparsed_data(url)[0]['changesPercentage']
+change = get_jsonparsed_data(url)[0]['change']
+
+data = "Name: {}\nPrice: {}\nChange: {}\nChanges Percentage: {}".format(name, price, change, changesPercentage)
+
+
  
 #Enter email information
-fromaddr = "yourEmail@gmail.com"
-password = "password"
-toaddr = ['to@gmail.com']
+fromaddr = "lynlynlynt@gmail.com"
+password = ""
+toaddr = ['mingxin.ou@gmail.com','centuryib100@gmail.com']
 # fakeCcList = ['fakecc@gmail.com']
 # bccList = ['bcc@gmail.com']
 displayName = 'Stock Master'
@@ -21,7 +42,8 @@ msg['From'] = formataddr((str(Header(displayName, 'utf-8')), fromaddr))
 
 #Edit subject, message, and reply-to information here
 msg['Subject'] = "Daily Stock Update"
-body = "The stock has just closed"
+body = "The stock market has just closed\n\n"
+body = body + data
 # msg.add_header('reply-to', "replyto@gmail.com")
 
 
